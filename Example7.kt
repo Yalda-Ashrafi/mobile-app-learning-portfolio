49345174227e6415f19439436b2ddd5f868b661e
Yalda-Ashrafi <yaldaashrafi54@gmail.com>
2026-08-28 20:26:06 +0800

    Delete Example7.kt

--- Example7.kt
+++
@@ -1,7 +0,0 @@
-fun displayAlertMessage(operatingSystem: String, emailId: String): String {
-    return "There is a new sign-in request on $operatingSystem for your Google Account $emailId."
-}
-
-fun main() {
-    println(displayAlertMessage("Windows", "student@example.com"))
-}
